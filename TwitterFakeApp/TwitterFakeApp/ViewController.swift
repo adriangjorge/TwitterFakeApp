0c0a9db6b26b648055d242bfed9f3a2185a5190c
Adrian G. Jorge <adrian.g.511@gmail.com>
2021-01-26 22:52:31 +0100

    First commit

---
+++ TwitterFakeApp/TwitterFakeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterFakeApp
+//
+//  Created by Adrián G. Jorge on 26/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
