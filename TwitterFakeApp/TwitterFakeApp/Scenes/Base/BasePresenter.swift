ca71c0d88566bd78fd90b635c254981ba340de74
Adrian G. Jorge <adrian.g.511@gmail.com>
2021-01-26 23:00:08 +0100

    Create BASE scene

---
+++ TwitterFakeApp/TwitterFakeApp/Scenes/Base/BasePresenter.swift
@@ -0,0 +1,10 @@
+//
+//  BasePresenter.swift
+//  TwitterFakeApp
+//
+//  Created by Adrián G. Jorge on 26/1/21.
+//
+
+class BasePresenter {
+    
+}
---
+++ TwitterFakeApp/TwitterFakeApp/Scenes/Base/BaseViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BaseViewController.swift
+//  TwitterFakeApp
+//
+//  Created by Adrián G. Jorge on 26/1/21.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    
+    
+}
